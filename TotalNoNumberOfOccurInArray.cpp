8a3ad93c3abf046f5454d9e8d102937ec37f3f7b
Himanshi Aggarwal <96831313+himanshi2003@users.noreply.github.com>
2023-03-07 23:35:00 +0530

    Add files via upload

---
+++ TotalNoNumberOfOccurInArray.cpp
@@ -0,0 +1,61 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+int leftOccur(int arr[],int n,int key){
+    int s=0;
+    int e=n-1;
+    int mid=s+(e-s)/2;
+    int ans=-1;
+    while(s<=e){
+        if(arr[mid]==key){
+            ans=mid;
+            //yha ham esa isliye kar rhe h kuki hame  left occur nikal rhe hai 
+            e=mid-1;
+        }
+        if(key>arr[mid]){
+            s=mid+1;
+        }
+        else if(key<arr[mid]){
+            e=mid-1;
+        }
+        mid=s+(e-s)/2;
+    }
+    return ans;
+        s=mid+1;
+    
+}
+int rightOccur(int arr[],int n,int key){
+    int s=0;
+    int e=n-1;
+    int mid=s+(e-s)/2;
+    int ans=-1;
+    while(s<=e){
+        if(arr[mid]==key){
+            ans=mid;
+            //yha ham esa isliye kar rhe h kuki hame  left occur nikal rhe hai 
+            s=mid+1;
+        }
+        if(key>arr[mid]){
+            s=mid+1;
+        }
+        else if(key<arr[mid]){
+            e=mid-1;
+        }
+        mid=s+(e-s)/2;
+    }
+    return ans;
+        s=mid+1;
+    
+}
+int main() {
+
+int even[6]={1,2,2,3,3,6};
+int first=leftOccur(even,6,3);
+int last=rightOccur(even,6,3);
+int totalNoOccurInArray=(last-first)+1;
+cout<<first<<endl;
+cout<<last<<endl;
+cout<<totalNoOccurInArray<<endl;
+    return 0;
+}
